7017a760e571e1c9a14067af23a16903013a00c0
Keirrison lucas <lucaskeirrison01@gmail.com>
2025-08-28 15:51:27 -0300

    salvando arquivos no github

---
+++ codigo keirrison/segundo codigo.py
@@ -0,0 +1,32 @@
+#print("keirrison"*8000)
+#exemplo de variados variavel do tipo texto
+fruta = "manga"
+print(type(fruta))
+#exemplo váriavel do tipo INT é inteiro, números inteiro
+idade = int(16)
+#exemplo de número quebrado, float
+nota = float(9.5)
+
+#booleana
+vc_curte = False
+
+#EXERCICIO 1 
+ 
+ #Criar uma váriavel que peça nome do usuario.
+nome = input("qual é o seu nome?")
+ #depois criar uma váriavel do tipo float, que pede a nota do usuário
+nota = float(input("Qual a sua nota?"))
+ #depois criar uma várivel  que peça a idade do usuário
+idade = int(input("Qual a sua idade ?"))
+ #depois de um print nas suas váriaveis
+print(nome)
+print(nota)
+print(idade)
+
+###EXERCICIO 2
+### crie uma váriavel que se chama nome_carro, coloque dentro dessa váriavel o seu nome dos seus sonhos
+nome_carro = "GTR"
+## crie uma váriavel que se chama preço, coloque nela a váriavel preço do carro
+preço = float(900.000)
+###crie uma váriavel que se chama ano, coloque dentro dessa váriavel o ano do carro
+ano = int(2025)
